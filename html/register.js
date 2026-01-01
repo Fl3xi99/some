723c4b4afd213d2c443f4eabac5628ea70a0b43e
Fl3xi99 <tvu1@telenet.be>
2026-01-01 18:40:44 +0100

    dd

---
+++ html/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCZpiXVtwrJAkcmy43_8bfsNdnG_Cm4ML0",
+    authDomain: "login-6b25d.firebaseapp.com",
+    projectId: "login-6b25d",
+    storageBucket: "login-6b25d.firebasestorage.app",
+    messagingSenderId: "249989946280",
+    appId: "1:249989946280:web:6cf30e3ee7c857fdfa1b9d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
